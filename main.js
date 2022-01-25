88a5ae369f7015c5c809733ae69e8ba4baefb316
katy millard <katy.millard@gmail.com>
2022-01-25 11:20:58 -0600

    First version

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const myStr = "hello class";
+
+console.log(myStr.replace('c', 'C'));
+console.log(myStr.replace('h', 'H'));
+console.log(myStr.concat('!'));
+console.log(myStr.toUpperCase());
+console.log(myStr.toLowerCase());
+console.log(myStr.repeat(5));
+console.log(myStr.replace('h', 'H').replace('c', 'C'));
+
+for (const value of myStr){ 
+    console.log(value)};
