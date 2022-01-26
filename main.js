f01d125a413c2dc20d8d1343fc2b43922702d798
katy millard <katy.millard@gmail.com>
2022-01-25 19:01:44 -0600

    Second commit. Added ssalc olleh.

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 
 const myStr = "hello class";
 
-console.log(myStr.replace('c', 'C'));
-console.log(myStr.replace('h', 'H'));
-console.log(myStr.concat('!'));
-console.log(myStr.toUpperCase());
-console.log(myStr.toLowerCase());
-console.log(myStr.repeat(5));
-console.log(myStr.replace('h', 'H').replace('c', 'C'));
+function main(){}
+    console.log(myStr.replace('c', 'C'));
+    console.log(myStr.replace('h', 'H'));
+    console.log(myStr.concat('!'));
+    console.log(myStr.toUpperCase());
+    console.log(myStr.toLowerCase());
+    console.log(myStr.repeat(5));
+    console.log(myStr.replace('h', 'H').replace('c', 'C'));
 
-for (const value of myStr){ 
-    console.log(value)};
+    for (let char of myStr) {
+        console.log(char);
+    }
+    
+    let result = '';
+    
+    for (let i = (myStr.length - 1); i >= 0; i--) {
+        result += myStr[i];
+    }
+    console.log(result);
+
+main();
